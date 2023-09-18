21886d611304088a35fb10b50a565b2dbe32aa40
Mert Ziya <108177583+mertziya@users.noreply.github.com>
2023-09-18 14:40:45 +0300

    Initial Commit

---
+++ H4CK3R/H4CK3RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4CK3RApp.swift
+//  H4CK3R
+//
+//  Created by Mert Ziya on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4CK3RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
